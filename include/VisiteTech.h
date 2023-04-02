43c1184b98e6d9d20774b83e4ebde26874a16537
abir.belh <abirbelhedi8@gmail.com>
2023-04-02 23:35:01 +0100

    hello

--- include/VisiteTech.h
+++ include/VisiteTech.h
@@ -1,13 +1,21 @@
 #ifndef VISITETECH_H
 #define VISITETECH_H
-
+#include<string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include"Test.h"
+using namespace std;
 
 class VisiteTech
 {
-    //string codeV;
-
+    string codeV;
+    vector <Test*> tests;
     public:
         VisiteTech();
+        friend ostream& operator<<(ostream& out, VisiteTech& V);
+        friend istream& operator>>(istream& in, VisiteTech& V);
+        void modifierVisite(string);
         virtual ~VisiteTech();
 
 };
--- src/VisiteTech.cpp
+++ src/VisiteTech.cpp
@@ -4,7 +4,20 @@ VisiteTech::VisiteTech()
 {
     //ctor
 }
+ostream& operator<<(ostream& out, VisiteTech& V)
+{
+    out<<"code : ";
+    out<<V.codeV;
+    out<<"table des visites ";
+    for(unsigned i=0;i<V.tests.size();i++)
+          out<<V.tests[i]<<'\t';
+          return out
+}
+istream& operator>>(istream& in, VisiteTech& V)
+{
+    in>>V.codeV;
 
+}
 VisiteTech::~VisiteTech()
 {
     //dtor
